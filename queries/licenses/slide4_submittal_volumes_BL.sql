75554d9ddf02b21a218f9a1dc4a4e2311ff8634d
Peter Dannemann <Peter.Dannemann@phila.gov>
2018-10-02 10:41:56 -0400

    Match number of licenses on Slide4BL with Slide1BL

--- queries/licenses/slide4_submittal_volumes_BL.sql
+++ queries/licenses/slide4_submittal_volumes_BL.sql
@@ -2,114 +2,118 @@
 --For business: April 2016- present
 --runtime 562.093 seconds
 SELECT DISTINCT
-     issuedate,
-     createdbytype,
-     jobtype,
-     licensetype,
-     COUNT(licensenumber) licensenumbercount
- FROM
-     (
-         SELECT
-             TO_DATE(issueyear
-                       || '/'
-                       || issuemonth
-                       || '/'
-                       || '01','yyyy/mm/dd') AS issuedate,
-             createdbytype,
-             jobtype,
-             licensetype,
-             licensenumber
-         FROM
-             (
-                 SELECT
-                     *
-                 FROM
-                     (
-                         ( SELECT DISTINCT
-                             ( CASE
-                                 WHEN ap.createdby LIKE '%2%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%3%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%4%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%5%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%6%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%7%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%7%' THEN 'Online'
-                                 WHEN ap.createdby LIKE '%9%' THEN 'Online'
-                                 WHEN ap.createdby = 'PPG User'                THEN 'Online'
-                                 WHEN ap.createdby = 'POSSE system power user' THEN 'Revenue'
-                                 ELSE 'Staff'
-                             END ) AS createdbytype,
-                             ( CASE
-                                 WHEN ap.applicationtype LIKE 'Application' THEN 'Application'
-                             END ) jobtype,
-                             lt.name             licensetype,
-                             lic.licensenumber   licensenumber,
-                             EXTRACT(MONTH FROM lic.mostrecentissuedate) issuemonth,
-                             EXTRACT(YEAR FROM lic.mostrecentissuedate) issueyear
-                         FROM
-                             lmscorral.bl_license lic,
-                             lmscorral.bl_licensetype lt,
-                             query.j_bl_application ap,
-                             query.r_bl_application_license apl,
-                             query.o_jobtypes jt
-                         WHERE
-                             lt.objectid = lic.licensetypeobjectid (+)
-                             AND lic.objectid = apl.licenseobjectid (+)
-                             AND apl.applicationobjectid = ap.objectid (+)
-                             AND ap.jobtypeid = jt.jobtypeid (+)
-                             AND ap.statusid LIKE '1036493'
-                             AND lic.initialissuedate > '01-APR-16'
-                             AND lic.initialissuedate < SYSDATE
-                             AND ap.applicationtype = 'Application'
-                         )
-                         UNION
-                         ( SELECT DISTINCT
-                             ( CASE
-                                 WHEN ar.createdby LIKE '%2%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%3%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%4%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%5%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%6%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%7%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%7%' THEN 'Online'
-                                 WHEN ar.createdby LIKE '%9%' THEN 'Online'
-                                 WHEN ar.createdby = 'PPG User'                THEN 'Online'
-                                 WHEN ar.createdby = 'POSSE system power user' THEN 'Revenue'
-                                 ELSE 'Staff'
-                             END ) AS createdbytype,
-                             ( CASE
-                                 WHEN ar.applicationtype LIKE 'Renewal' THEN 'Renewal'
-                             END ) jobtype,
-                             lt.name             licensetype,
-                             lic.licensenumber   licensenumber,
-                             EXTRACT(MONTH FROM lic.mostrecentissuedate) issuemonth,
-                             EXTRACT(YEAR FROM lic.mostrecentissuedate) issueyear
-                         FROM
-                             lmscorral.bl_licensetype lt,
-                             lmscorral.bl_license lic,
-                             query.r_bl_amendrenew_license arl,
-                             query.j_bl_amendrenew ar,
-                             query.o_jobtypes jt
-                         WHERE
-                             lt.objectid = lic.licensetypeobjectid (+)
-                             AND lic.objectid = arl.licenseid (+)
-                             AND arl.amendrenewid = ar.objectid (+)
-                             AND ar.jobtypeid = jt.jobtypeid (+)
-                             AND ar.statusid LIKE '1036493'
-                             AND ar.issuedate > '01-APR-16'
-                             AND ar.issuedate < SYSDATE
-                             AND ar.applicationtype LIKE 'Renewal'
-                         )
-                     )
-             )
-     )
- GROUP BY
-     issuedate,
-     createdbytype,
-     jobtype,
-     licensetype
- ORDER BY
-     issuedate,
-     createdbytype,
-     jobtype,
-     licensetype
+    issuedate,
+    createdbytype,
+    jobtype,
+    licensetype,
+    COUNT(DISTINCT licensenumber) licensenumbercount
+FROM
+    (
+        SELECT
+            TO_DATE(issueyear
+                      || '/'
+                      || issuemonth
+                      || '/'
+                      || '01','yyyy/mm/dd') AS issuedate,
+            createdbytype,
+            jobtype,
+            licensetype,
+            licensenumber
+        FROM
+            (
+                SELECT
+                    *
+                FROM
+                    (
+                        ( SELECT DISTINCT
+                            (
+                                CASE
+                                    WHEN ap.createdby LIKE '%2%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%3%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%4%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%5%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%6%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%7%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%7%' THEN 'Online'
+                                    WHEN ap.createdby LIKE '%9%' THEN 'Online'
+                                    WHEN ap.createdby = 'PPG User'                THEN 'Online'
+                                    WHEN ap.createdby = 'POSSE system power user' THEN 'Revenue'
+                                    ELSE 'Staff'
+                                END
+                            ) AS createdbytype,
+                            (
+                                CASE
+                                    WHEN ap.applicationtype LIKE 'Application' THEN 'Application'
+                                END
+                            ) jobtype,
+                            lt.name licensetype,
+                            lic.externalfilenum licensenumber,
+                            EXTRACT(MONTH FROM ap.issuedate) issuemonth,
+                            EXTRACT(YEAR FROM ap.issuedate) issueyear
+                          FROM
+                            query.j_bl_application ap,
+                            query.r_bl_application_license apl,
+                            query.o_bl_license lic,
+                            lmscorral.bl_licensetype lt
+                          WHERE
+                            lic.licensetypeid = lt.objectid
+                            AND lic.objectid = apl.licenseobjectid
+                            AND apl.applicationobjectid = ap.jobid
+                            AND ap.statusdescription LIKE 'Approved'
+                            AND lic.initialissuedate > '01-APR-16'
+                            AND lic.initialissuedate < SYSDATE
+                            AND ap.applicationtype = 'Application'
+                        )
+                        UNION
+                        ( SELECT DISTINCT
+                            (
+                                CASE
+                                    WHEN ar.createdby LIKE '%2%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%3%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%4%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%5%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%6%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%7%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%7%' THEN 'Online'
+                                    WHEN ar.createdby LIKE '%9%' THEN 'Online'
+                                    WHEN ar.createdby = 'PPG User'                THEN 'Online'
+                                    WHEN ar.createdby = 'POSSE system power user' THEN 'Revenue'
+                                    ELSE 'Staff'
+                                END
+                            ) AS createdbytype,
+                            (
+                                CASE
+                                    WHEN ar.applicationtype LIKE 'Renewal' THEN 'Renewal'
+                                END
+                            ) jobtype,
+                            lt.name licensetype,
+                            lic.externalfilenum licensenumber,
+                            EXTRACT(MONTH FROM ar.issuedate) issuemonth,
+                            EXTRACT(YEAR FROM ar.issuedate) issueyear
+                          FROM
+                            query.j_bl_amendrenew ar,
+                            query.r_bl_amendrenew_license arl,
+                            query.o_bl_license lic,
+                            lmscorral.bl_licensetype lt
+                          WHERE
+                            lic.licensetypeid = lt.objectid
+                            AND lic.objectid = arl.licenseid
+                            AND arl.amendrenewid = ar.jobid
+                            AND ar.statusdescription LIKE 'Approved'
+                            AND ar.issuedate > '01-APR-16'
+                            AND ar.issuedate < SYSDATE
+                            AND ar.applicationtype LIKE 'Renewal'
+                        )
+                    )
+            )
+    )
+GROUP BY
+    issuedate,
+    createdbytype,
+    jobtype,
+    licensetype
+ORDER BY
+    issuedate,
+    createdbytype,
+    jobtype,
+    licensetype

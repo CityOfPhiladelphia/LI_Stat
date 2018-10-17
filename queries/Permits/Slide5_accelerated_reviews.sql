114207d8bdc06b0f49e36e09004ba7c201fa7528
Philip Ribbens <Philip.Ribbens@phila.gov>
2018-10-17 13:44:10 -0400

    More progress on permits slide 5 query

--- queries/Permits/Slide5_accelerated_reviews.sql
+++ queries/Permits/Slide5_accelerated_reviews.sql
@@ -1,6 +1,6 @@
-SELECT b.apno,
-  b.APDTTM ApplicationDate,
-  b.issdttm IssueDate,
+SELECT distinct b.apno,
+  b.APDTTM PermitApplicationDate,
+  b.issdttm PermitIssueDate,
   (CASE
       WHEN b.issdttm - b.APDTTM <= 10
       THEN 'Within SLA'
@@ -10,15 +10,17 @@ SELECT b.apno,
   defn.aptype PermitType,
   b.worktype,
   act.ISSDTTM ReviewIssueDate,
-  f.stat FeePaidStatus,
-  f.feedesc
+  f.feedesc,
+  rec.hours
 FROM imsv7.apbldg b,
   imsv7.apact act,
   imsv7.apdefn defn,
-  imsv7.apfee f
+  imsv7.apfee f,
+  imsv7.aprec rec
 WHERE b.apdefnkey = defn.apdefnkey
 AND b.APKEY       = act.APKEY (+)
-AND b.apkey       = f.apkey
+AND b.apkey       = f.apkey (+)
+AND b.apkey       = rec.apkey (+)
 AND b.APDTTM     >= '01-JAN-2016'
 AND f.stat        = 'P'
 AND (f.feedesc LIKE '%ACCELERATED%' OR f.FEEDESC LIKE '%ACCELARATED%')

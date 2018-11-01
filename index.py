92f7f2db63414a5eb4551696f23c646b543d0d47
Philip Ribbens <Philip.Ribbens@phila.gov>
2018-11-01 13:41:25 -0400

    Get server stuff right

--- index.py
+++ index.py
@@ -66,6 +66,6 @@ def display_page(pathname):
         return Slide1BL.layout
 
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port=5001)
-    # http_server = WSGIServer(('0.0.0.0', 5000), server)
-    # http_server.serve_forever()
+    #app.run_server(host='127.0.0.1', port=5001)
+    http_server = WSGIServer(('0.0.0.0', 5000), server)
+    http_server.serve_forever()

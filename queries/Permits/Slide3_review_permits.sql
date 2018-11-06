4e6b3613348d5cd3460ffdff29c6b31074490337
Shannon Holm <shannon.Holm@phila.gov>
2018-11-06 11:35:48 -0500

    updating Slide3_review_permits.sql with RPTPER log

--- queries/Permits/Slide3_review_permits.sql
+++ queries/Permits/Slide3_review_permits.sql
@@ -1,27 +1,34 @@
 SELECT trim(b.apno) PermitNumber,
   b.APDTTM ProcessingDate,
-  nvl(act.issdttm, b.issdttm) PermitIssueDate,
+  nvl(nvl(aprec.stopdttm, aprec.startdttm), b.issdttm) PermitIssueDate,
   defn.aptype PermitType,
   b.worktype
 FROM imsv7.apbldg b,
   imsv7.apact act,
+  imsv7.aprec aprec,
   imsv7.apdefn defn
 WHERE b.apdefnkey                    = defn.apdefnkey
 AND TO_DATE(b.APDTTM, 'YYYY-MM-DD') != TO_DATE(b.ISSDTTM, 'YYYY-MM-DD')
 AND b.APKEY                          = act.APKEY (+)
+AND b.APKEY                          = aprec.APKEY (+)
 AND b.APDTTM                        >= '01-JAN-2016'
 AND b.parentkey                      = '1'
+--and upper(a.comments) like '%ISSUE%'
+and aprec.logtype like 'RPTPER%'
+
 UNION
 SELECT trim(u.apno) PermitNumber,
   u.APDTTM ProcessingDate,
-  nvl(act.issdttm, u.issdttm) PermitIssueDate,
+  nvl(nvl(aprec.stopdttm, aprec.startdttm), u.issdttm) PermitIssueDate,
   defn.aptype PermitType,
   u.worktype
 FROM imsv7.apuse u,
   imsv7.apact act,
+  imsv7.aprec aprec,
   imsv7.apdefn defn
 WHERE u.apdefnkey                    = defn.apdefnkey
 AND TO_DATE(u.APDTTM, 'YYYY-MM-DD') != TO_DATE(u.ISSDTTM, 'YYYY-MM-DD')
 AND u.APKEY                          = act.APKEY (+)
+AND u.Apkey                          = aprec.apkey (+)
 AND u.APDTTM                        >= '01-JAN-2016'
 AND u.parentkey                      = '1'
